f8dcd8692a2bbb06207958920dbc3eead6884542
OMOMOH <omomohagiogu@gmail.com>
2023-01-13 19:00:01 +0100

    my firstcommitt of the day

--- my-newlater-form-pluging.php
+++ my-newlater-form-pluging.php
@@ -7,15 +7,23 @@ Version: 1.0
 Author: Omomoh
 */
 
-function hello_world_shortcode() {
-    return "Hello World";
+
+function mynewslater_form_function() {
+  $form ='<form>'; 
+  $form .= '<h1>Newsletter Signup</h1>';
+  $form .= '<p>No spam, unsubscribe at any time.</p>';
+  $form .= '<div class="info">';
+  $form .= '<input type="text" placeholder="Your Name">';
+  $form .= '<input type="email" placeholder="Your Email">';
+  $form .= '</div>';
+  $form .= '<input type="submit" value="Subscribe">';
+  $form .= '</form>';
+  return $form;
 }
-add_shortcode( 'hello_world', 'hello_world_shortcode' );
+add_shortcode( 'my_form', 'mynewslater_form_function' );
 
-
-
-function hello_world_assets() {
-    wp_enqueue_style( 'hello-world', plugin_dir_url( __FILE__ ) . 'assets/css/hello-world.css' );
-    wp_enqueue_script( 'hello-world', plugin_dir_url( __FILE__ ) . 'assets/js/hello-world.js', array( 'jquery' ), '1.0', true );
+function news_assets() {
+    wp_enqueue_style( 'news-letters', plugin_dir_url( __FILE__ ) . 'assets/css/main.css' );
+    wp_enqueue_script( 'news-letters', plugin_dir_url( __FILE__ ) . 'assets/js/hello-world.js', array( 'jquery' ), '1.0', true );
 }
-add_action( 'wp_enqueue_scripts', 'hello_world_assets' );
+add_action( 'wp_enqueue_scripts', 'news_assets' );
